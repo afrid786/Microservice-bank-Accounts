7eaf92ff9daeb383d38ddb377745551d9ce082f1
afrid786 <jobsforafrid@gmail.com>
2023-09-26 20:03:43 +0530

    Swagger API documentation changes

--- src/main/java/com/afrid/bank/controller/AccountController.java
+++ src/main/java/com/afrid/bank/controller/AccountController.java
@@ -5,6 +5,10 @@ import com.afrid.bank.constants.AccountsConstants;
 import com.afrid.bank.dto.CustomerDTO;
 import com.afrid.bank.dto.ResponseDTO;
 import com.afrid.bank.service.IAccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -14,6 +18,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "CRUD API for bank account microservice",
+        description = "This API handles CRUD operations in bank - account microservices"
+)
 @RestController
 @RequestMapping(path = "api/v1", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
@@ -22,6 +30,14 @@ public class AccountController {
 
     private IAccountsService iAccountsService;
 
+    @Operation(
+            summary = "Create account API",
+            description = "Rest API to create to bank account for user"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP status created"
+    )
     @PostMapping(path = "/create")
     public ResponseEntity<ResponseDTO> createAccount (@Valid @RequestBody CustomerDTO customerDTO) {
 
@@ -31,6 +47,14 @@ public class AccountController {
                 .body(new ResponseDTO(AccountsConstants.STATUS_200,AccountsConstants.MESSAGE_200));
     }
 
+    @Operation(
+            summary = "GET API for returning account details",
+            description = "Rest API to get bank account details"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP status successful"
+    )
     @GetMapping(path = "/account")
     public ResponseEntity<CustomerDTO> fetchAccountDetails (@Pattern(regexp = "($|[0-9]{10})",message = "Mobile  number is not valid")
                                                                 @RequestParam String mobileNumber) {
@@ -40,6 +64,17 @@ public class AccountController {
 
     }
 
+    @Operation(
+            summary = "Update API for returning account details",
+            description = "Rest API to update bank account details"
+    )
+    @ApiResponses({
+                    @ApiResponse(  responseCode = "200",
+                            description = "HTTP status created"),
+                    @ApiResponse(  responseCode = "500",
+                            description = "HTTP status Internal server Error")
+
+            })
     @PutMapping("/updateAccount")
     public ResponseEntity<ResponseDTO> updateAccountDetails (@Valid @RequestBody CustomerDTO customerDTO) {
         boolean isAccountUpdated = iAccountsService.updateAccount(customerDTO);
@@ -52,6 +87,17 @@ public class AccountController {
         }
     }
 
+    @Operation(
+            summary = "Delete API for returning account details",
+            description = "Rest API to delete bank account details"
+    )
+    @ApiResponses({
+            @ApiResponse(  responseCode = "200",
+                    description = "HTTP status created"),
+            @ApiResponse(  responseCode = "500",
+                    description = "HTTP status Internal server Error")
+
+    })
     @DeleteMapping("/deleteAccount")
     public ResponseEntity<ResponseDTO> deleteAccount (@Pattern(regexp = "($|[0-9]{10})",message = "Mobile  number is not valid")
                                                           @RequestParam String mobileNumber) {
--- src/main/java/com/afrid/bank/dto/AccountDTO.java
+++ src/main/java/com/afrid/bank/dto/AccountDTO.java
@@ -1,10 +1,12 @@
 package com.afrid.bank.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Schema(name = "Account")
 public class AccountDTO {
 
     @NotEmpty(message = "AccountNumber can't be empty")
