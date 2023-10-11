5b4115ec97454e1bb61ed5d65c1cb96db9175cb9
afrid786 <jobsforafrid@gmail.com>
2023-10-11 18:42:16 +0530

    1. Added google jib plugin to build docker image 2. Added spring config client changes

--- src/main/java/com/afrid/bank/BankApplication.java
+++ src/main/java/com/afrid/bank/BankApplication.java
@@ -1,15 +1,18 @@
 package com.afrid.bank;
 
+import com.afrid.bank.dto.AccountsContactInfo;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditaware")
+@EnableConfigurationProperties(value = {AccountsContactInfo.class})
 @OpenAPIDefinition(info = @Info(
 		title = "Account Microservice API Documentation",
 		description = "Bank's account microservice swagger api documentation",
